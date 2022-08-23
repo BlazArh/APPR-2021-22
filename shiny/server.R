3407286d239d6ddccf580fd5bec291d2851f10d5
Blaž Arh <b.blazarh@gmail.com>
2022-08-23 09:24:29 +0200

    uvoz, shiny

--- shiny/server.R
+++ shiny/server.R
@@ -1,2 +1,29 @@
 library(shiny)
+library(reshape2)
+library(data.table)
+library(DT)
+
+SkupnaTabela$Country <- factor(SkupnaTabela$Country,levels=SkupnaTabela %>% arrange(Dohodek.2019) %>% .$Country, 
+                      ordered = TRUE) 
+
+shinyServer(function(input, output) { 
+  
+  output$izbor_stolpcev = renderUI({ 
+    conditionalPanel( 
+      'input.dataset === "tab_skupna"', 
+      checkboxGroupInput("show_vars", "Kateri stolpec naj prika?e:",
+                         names(SkupnaTabela), selected = names(SkupnaTabela)) 
+    ) 
+  }) 
+  output$izbor_drzav = renderUI({ 
+    conditionalPanel( 
+      'input.dataset === "tab_place"', 
+      checkboxGroupInput("izbrane_drzave", "Izberi drzave:", 
+                         TabelaPlace$Drzava, selected = c("Slovenia")) 
+    ) 
+  }) 
+  
+  
+  output$tabelaSkupna <- DT::renderDataTable({DT::datatable(SkupnaTabela %>% select(input$show_vars))}) 
+})
 
