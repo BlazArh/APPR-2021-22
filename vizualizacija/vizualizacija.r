078fd341c902a600bf55d5ac2382474bf2e7db42
Blaž Arh <b.blazarh@gmail.com>
2022-08-24 10:00:32 +0200

    boljša preglednost

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -17,12 +17,21 @@ print(graf1)
 # spremeni barvo!
 
 
-
+#-------------------------------------------------------------------------------
 
 # 7 izbranih držav, čez leta kako kriminal narašča
 drzave2 <- c("Slovenia", "Austria", "Italy", "Germany", "France", "Iceland", "Sweden", "Greece")
 #tabela2 <- Varnost %>% filter(Drzava %in% drzave2) %>% filter(indeks == "Kriminal")
 tabela2 <- skupna %>% filter(Drzava %in% drzave2) %>% filter(tip == "kriminal")
+
+
+graf2 <- ggplot(tabela2, aes(x = Leto, y = vrednost, color = Drzava)) + geom_line()+
+  xlab("Leto") + ylab("Indeks kriminala") 
+
+print(graf2)
+
+#-------------------------------------------------------------------------------
+#primerjam Slovenijo in Švic0 (dohodek izobrazba, populacija)
 
 graf_primerjava <- function(drzava, tip_podatka, naslov){
   tabela <- skupna %>% filter(Drzava == drzava)
@@ -33,23 +42,18 @@ graf_primerjava <- function(drzava, tip_podatka, naslov){
 }
 
 
-
 graf3 <- graf_primerjava("Slovenia", "dohodek", "Dohodek")
-
 graf4 <- graf_primerjava("Slovenia", "populacija", "Število prebivalcev")
-
 graf5 <- graf_primerjava("Slovenia", "izobrazba", "Indeks izobrazbe")
-
 graf6 <- graf_primerjava("Switzerland", "dohodek", "Dohodek")
-
 graf7 <- graf_primerjava("Switzerland", "populacija", "Število prebivalcev")
-
 graf8 <- graf_primerjava("Switzerland", "izobrazba", "Indeks izobrazbe")
 
-skupek1 <- plot_grid(graf3, graf4, graf5, graf6, graf7, graf8)
 
+skupek1 <- plot_grid(graf3, graf4, graf5, graf6, graf7, graf8)
 print(skupek1)
 
+#-------------------------------------------------------------------------------
 
 # dohodek, varnost za leto 2013
 tabela5 <- skupna %>% filter(Leto  == 2013 & tip %in% c("varnost", "dohodek"))
@@ -60,6 +64,7 @@ gr_DV_2013 <- ggplot(tabela5, aes(x=dohodek, y = varnost))+
   geom_smooth(method = loess) +
   ggtitle("2013") + xlab("Visina place") + ylab("Indeks varnosti")
 
+#dohodek, varnost za leto 2015
 tabela6 <- skupna %>% filter(Leto  == 2015 & tip %in% c("varnost", "dohodek"))
 tabela6 <- tabela6 %>% pivot_wider( names_from = tip, values_from = vrednost)
 
@@ -68,7 +73,7 @@ gr_DV_2015 <- ggplot(tabela6, aes(x=dohodek, y = varnost))+
   geom_smooth(method = loess) +
   ggtitle("2015") + xlab("Visina place") + ylab("Indeks varnosti")
 
-
+#dohodek, varnost za leto 2017
 tabela7 <- skupna %>% filter(Leto  == 2017 & tip %in% c("varnost", "dohodek"))
 tabela7 <- tabela7 %>% pivot_wider( names_from = tip, values_from = vrednost)
 
@@ -77,6 +82,7 @@ gr_DV_2017 <- ggplot(tabela7, aes(x=dohodek, y = varnost))+
   geom_smooth(method = loess) +
   ggtitle("2017") + xlab("Visina place") + ylab("Indeks varnosti")
 
+#dohodek, varnost za leto 2019
 tabela8 <- skupna %>% filter(Leto  == 2019 & tip %in% c("varnost", "dohodek"))
 tabela8 <- tabela8 %>% pivot_wider( names_from = tip, values_from = vrednost)
 
@@ -85,10 +91,11 @@ gr_DV_2019 <- ggplot(tabela5, aes(x=dohodek, y = varnost))+
   geom_smooth(method = loess) +
   ggtitle("2019") + xlab("Visina place") + ylab("Indeks varnosti")
 
+#združim v en graf
 skupek2 <- plot_grid(gr_DV_2013, gr_DV_2015, gr_DV_2017, gr_DV_2019)
 print(skupek2)
 
-
+#-------------------------------------------------------------------------------
 
 
 # populacija, kriminal za leto 2013
@@ -102,7 +109,7 @@ gr_PK_2013 <- ggplot(tabela9, aes(x=populacija, y = kriminal))+
 
 gr_PK_2013
 
-
+# populacija, kriminal za leto 2015
 tabela10 <- skupna %>% filter(Leto  == 2015 & tip %in% c("kriminal", "populacija"))
 tabela10 <- tabela10 %>% pivot_wider( names_from = tip, values_from = vrednost)
 
@@ -113,7 +120,7 @@ gr_PK_2015 <- ggplot(tabela10, aes(x=populacija, y = kriminal))+
 
 gr_PK_2015
 
-
+# populacija, kriminal za leto 2017
 tabela11 <- skupna %>% filter(Leto  == 2017 & tip %in% c("kriminal", "populacija"))
 tabela11 <- tabela11 %>% pivot_wider( names_from = tip, values_from = vrednost)
 
@@ -124,6 +131,7 @@ gr_PK_2017 <- ggplot(tabela11, aes(x=populacija, y = kriminal))+
 
 gr_PK_2017
 
+# populacija, kriminal za leto 2019
 tabela12 <- skupna %>% filter(Leto  == 2019 & tip %in% c("kriminal", "populacija"))
 tabela12 <- tabela12 %>% pivot_wider( names_from = tip, values_from = vrednost)
 
@@ -134,14 +142,64 @@ gr_PK_2019 <- ggplot(tabela12, aes(x=populacija, y = kriminal))+
 
 gr_PK_2019
 
+
+#združim 4 grafe v en graf
 skupek3 <- plot_grid(gr_PK_2013, gr_PK_2015, gr_PK_2017, gr_PK_2019)
 print(skupek3)
 
 
+#-------------------------------------------------------------------------------
+
+# dohodek, populacija za leto 2013
+tabela13 <- skupna %>% filter(Leto  == 2013 & tip %in% c("dohodek", "populacija"))
+tabela13 <- tabela13 %>% pivot_wider( names_from = tip, values_from = vrednost)
+
+gr_DP_2013 <- ggplot(tabela13, aes(x=dohodek , y = populacija))+
+  geom_point() +
+  geom_smooth(method = loess) +
+  ggtitle("2013") + xlab("Dohodek") + ylab("Število prebivalcev")
+
+gr_DP_2013
+
+# dohodek, populacija za leto 2015
+tabela14 <- skupna %>% filter(Leto  == 2015 & tip %in% c("dohodek", "populacija"))
+tabela14 <- tabela14 %>% pivot_wider( names_from = tip, values_from = vrednost)
+
+gr_DP_2015 <- ggplot(tabela14, aes(x=dohodek , y = populacija))+
+  geom_point() +
+  geom_smooth(method = loess) +
+  ggtitle("2015") + xlab("Dohodek") + ylab("Število prebivalcev")
+
+gr_DP_2015
+
+# dohodek, populacija za leto 2017
+tabela15 <- skupna %>% filter(Leto  == 2017 & tip %in% c("dohodek", "populacija"))
+tabela15 <- tabela15 %>% pivot_wider( names_from = tip, values_from = vrednost)
+
+gr_DP_2017 <- ggplot(tabela13, aes(x=dohodek , y = populacija))+
+  geom_point() +
+  geom_smooth(method = loess) +
+  ggtitle("2017") + xlab("Dohodek") + ylab("Število prebivalcev")
+
+gr_DP_2017
+
+# dohodek, populacija za leto 2019
+tabela16 <- skupna %>% filter(Leto  == 2013 & tip %in% c("dohodek", "populacija"))
+tabela16 <- tabela16 %>% pivot_wider( names_from = tip, values_from = vrednost)
+
+gr_DP_2019 <- ggplot(tabela16, aes(x=dohodek , y = populacija))+
+  geom_point() +
+  geom_smooth(method = loess) +
+  ggtitle("2019") + xlab("Dohodek") + ylab("Število prebivalcev")
+
+gr_DP_2019
 
 
+#zdužim 4 grafe v enega
+skupek4 <- plot_grid(gr_DP_2013, gr_DP_2015, gr_DP_2017, gr_DP_2019)
+print(skupek4)
 
-
+#-------------------------------------------------------------------------------
 
 # Uvozimo zemljevid.
 zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m_cultural.zip",
